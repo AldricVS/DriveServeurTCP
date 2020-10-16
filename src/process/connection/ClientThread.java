72e84138a8a198914604b86a4e073d0d73a92d38
Aldric Vitali Silvestre <Aldric Vitali Silvestre@192.168.1.14>
2020-10-16 09:50:25 +0200

    handling multiple clients connection (not finished yet)

--- src/process/connection/ClientThread.java
+++ src/process/connection/ClientThread.java
@@ -1,6 +1,14 @@
 package process.connection;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
+
+import org.apache.log4j.Logger;
+
+import logger.LoggerUtility;
 
 
 /**
@@ -9,13 +17,43 @@ import java.net.Socket;
  * @author Aldric Vitali Silvestre <aldric.vitali@outtlok.fr>
  */
 public class ClientThread extends Thread {
-	Socket clientSocket;
+	private static Logger logger = LoggerUtility.getLogger(ClientThread.class, LoggerUtility.LOG_PREFERENCE);
+	
+	private Socket clientSocket;
+	
+	/**
+	 * We keep trace of the handler of all clients thread in order to call his methods
+	 */
+	private ThreadsConnectionHandler handler;
+	
+	/**
+	 * To communicate with client
+	 */
+	private PrintWriter outputFlow;
+	private BufferedReader inputFlow;
 	
 	/**
 	 * Make the connection with client, but user is not logged in yet
 	 * @param clientSocket
+	 * @param ThreadsConnectionHandler
 	 */
-	public ClientThread(Socket clientSocket) {
+	public ClientThread(Socket clientSocket, ThreadsConnectionHandler threadsConnectionHandler){
 		this.clientSocket = clientSocket;
+		this.handler = threadsConnectionHandler;
+	}
+	
+	@Override
+	public void run() {
+		try {
+			inputFlow = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+			outputFlow = new PrintWriter(clientSocket.getOutputStream(), true);
+			
+			
+			
+		}catch (IOException e) {
+			String errorMessage = "Error while communicating with client : " + e.getMessage();
+			ClientThread.logger.error(errorMessage);
+			System.err.println(errorMessage);
+		}
 	}
 }
--- src/process/connection/ThreadsConnectionHandler.java
+++ src/process/connection/ThreadsConnectionHandler.java
@@ -1,5 +1,8 @@
 package process.connection;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,24 +10,72 @@ import org.apache.log4j.Logger;
 
 import data.User;
 import logger.LoggerUtility;
+import process.database.DatabaseManager;
 
 /**
  * Main class of the server : it will wait for new clients connecting and create threads for newcommers.
  * @author Aldric Vitali Silvestre <aldric.vitali@outlook.fr>
  */
-public class ThreadsConnectionHandler {
+public class ThreadsConnectionHandler extends Thread{
 	private static Logger logger = LoggerUtility.getLogger(ThreadsConnectionHandler.class, LoggerUtility.LOG_PREFERENCE);
 	
+	private boolean isListening = true;
+	
+	/**
+	 * We store user list in order to keep a trace of all users (useful for checking if any user want to connect twice for exemple)
+	 */
 	private List<User> users = new ArrayList<>(); 
+	
+	private ServerSocket serverSocket;
+	
+	private DatabaseManager databaseManager;
 	
 	/**
 	 * Creating an instance of the class will start listenning for new clients
+	 * @param port the port where to listen for clients
 	 */
-	public ThreadsConnectionHandler() {
+	public ThreadsConnectionHandler(int port, String databaseUrl, String databaseUser, String databasePassword) {
+		try {
+			//connect to database
+			databaseManager = new DatabaseManager(databaseUrl, databaseUser, databasePassword);
+			serverSocket = new ServerSocket(port);
+			System.out.println("Waiting for clients on port " + port);
+			logger.info("Server waiting for clients on port " + port);
+		}catch (IOException e) {
+			//we can't do anything here, we have to stop the application  
+			String errorMessage = "Cannot connect on port " + port + " : " + e.getMessage();
+			System.err.println(errorMessage);
+			logger.fatal(errorMessage);
+			System.exit(-1);
+		}
+		catch (SQLException e) {
+			//we can't do anything here, we have to stop the application  
+			String errorMessage = "Cannot connect to database : " + e.getMessage();
+			System.err.println(errorMessage);
+			logger.fatal(errorMessage);
+			System.exit(-1);
+		}
 		
+		//here, the thread will start end witing for new clients
 	}
 	
-	public void handleNewClient() {
-		
+	@Override
+	public void run() {
+		while(isListening) {
+			try {
+				//start a new thread for the client
+				ClientThread clientThread = new ClientThread(serverSocket.accept(), this);
+				clientThread.start();
+				String message = "New client connected";
+				logger.info(message);
+				System.out.println(message);
+			} catch (IOException e) {
+				//error happened, stop communications
+				String errorMessage = "Error while listening for new clients : " + e.getMessage();
+				logger.error(errorMessage);
+				System.err.println(errorMessage);
+				isListening = false;
+			}
+		}
 	}
 }
